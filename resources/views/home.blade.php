957592a0120212467e28094ca83edd3b46b5f975
sofiacottone <cottonesofia@outlook.it>
2024-05-21 16:44:57 +0200

    feat: print movies from db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,30 @@
 @extends('layouts.app')
+
+@section('main-content')
+    <div class="container">
+        <div class="row row-cols-3">
+            @foreach ($movies as $movie)
+                <div class="ms-flip-card col-xxl-3 col-lg-4 col-md-6 col-sm-9 mb-4 mx-auto">
+
+                    <div class="ms-flip-card-inner border">
+
+                        <div class="ms-flip-card-front">
+                            <img src="{{ $movie['thumb'] }}">
+                        </div>
+
+                        <div class="ms-flip-card-back p-3">
+                            <div><span>Title: </span>{{ $movie->title }}</div>
+                            <div><span>Original Title: </span>{{ $movie->original_title }}</div>
+                            <div><span>Nationality: </span>{{ $movie->nationality }}</div>
+                            <div><span>Date: </span>{{ $movie->date }}</div>
+                            <div><span>Vote: </span>{{ $movie->vote }}</div>
+
+                        </div>
+
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+@endsection
