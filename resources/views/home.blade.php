bb0df393fc022f9917c13057932ab7f632882b9d
sofiacottone <cottonesofia@outlook.it>
2024-05-21 17:02:23 +0200

    feat: minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,33 @@
 @extends('layouts.app')
 
 @section('main-content')
-    <div class="container">
-        <div class="row row-cols-3">
-            @foreach ($movies as $movie)
-                <div class="ms-flip-card col-xxl-3 col-lg-4 col-md-6 col-sm-9 mb-4 mx-auto">
+    <div class="container-fluid bg-dark-subtle">
+        <div class="container pt-5">
+            <div class="row row-cols-3">
 
-                    <div class="ms-flip-card-inner border">
+                @foreach ($movies as $movie)
+                    <div class="ms-flip-card col-xxl-3 col-lg-4 col-md-6 col-sm-9 mb-4 mx-auto">
 
-                        <div class="ms-flip-card-front">
-                            <img src="{{ $movie['thumb'] }}">
+                        <div class="ms-flip-card-inner">
+
+                            <div
+                                class="ms-flip-card-front d-flex justify-content-center align-items-center text-uppercase fw-bold">
+                                <div>{{ $movie->title }}</div>
+                            </div>
+
+                            <div class="ms-flip-card-back p-3">
+                                <div><span>Title: </span>{{ $movie->title }}</div>
+                                <div><span>Original Title: </span>{{ $movie->original_title }}</div>
+                                <div><span>Nationality: </span>{{ $movie->nationality }}</div>
+                                <div><span>Date: </span>{{ $movie->date }}</div>
+                                <div><span>Vote: </span>{{ $movie->vote }}</div>
+                            </div>
+
                         </div>
-
-                        <div class="ms-flip-card-back p-3">
-                            <div><span>Title: </span>{{ $movie->title }}</div>
-                            <div><span>Original Title: </span>{{ $movie->original_title }}</div>
-                            <div><span>Nationality: </span>{{ $movie->nationality }}</div>
-                            <div><span>Date: </span>{{ $movie->date }}</div>
-                            <div><span>Vote: </span>{{ $movie->vote }}</div>
-
-                        </div>
-
                     </div>
-                </div>
-            @endforeach
+                @endforeach
 
+            </div>
         </div>
     </div>
 @endsection
